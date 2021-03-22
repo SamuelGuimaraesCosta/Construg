50d0a466137420053325d1d1f7384a02af7f7bf4
SamuelGuimaraesCosta <75049214+SamuelGuimaraesCosta@users.noreply.github.com>
2021-03-22 19:55:14 -0300

    clean the code 1

--- src/server.js
+++ src/server.js
@@ -3,11 +3,9 @@ var http = require('http');
 var port = process.env.PORT || 3333;
 const express = require("express");
 const bodyParser = require("body-parser");
-const sqlite3 = require("sqlite3").verbose();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const mysql = require('mysql');
-const fs = require("fs");
 
 const app = express();
 const router = express.Router();
