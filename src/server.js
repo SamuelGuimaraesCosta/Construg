09dfe469bd9aec300a53bb5441c6c54ac7f3f90d
SamuelGuimaraesCosta <75049214+SamuelGuimaraesCosta@users.noreply.github.com>
2021-03-31 19:23:54 -0300

    Update server.js
    minor_changes

--- src/server.js
+++ src/server.js
@@ -25,9 +25,7 @@ router.get('/', (req, res) => {
 app.use(router);
 
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
