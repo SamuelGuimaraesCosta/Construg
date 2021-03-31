ee788b484d4373c11f9dd1d0461ae58fcd8e4678
SamuelGuimaraesCosta <75049214+SamuelGuimaraesCosta@users.noreply.github.com>
2021-03-31 19:40:30 -0300

    Update server.js
    login_route_1

--- src/server.js
+++ src/server.js
@@ -18,8 +18,59 @@ var conn = mysql.createConnection({
     port: 3306
 });
 
+conn.connect(function (err) {
+    if (err)
+        return console.log("Falha na conexão com BD: " + err);
+    return console.log("Conexão com BD realizada com sucesso! " + mysqlConn.threadId);
+});
+
+const findUserByEmail = (email, cb) => {
+    conn.query(`SELECT IDUSUARIO, NOME, EMAIL, SENHA, DTNASCIMENTO, ATIVO, BLOQUEADO, MASTER, CPF FROM USUARIOS WHERE EMAIL = ?`, [email], function (error, result) {
+        if (typeof result !== 'undefined' && result.length > 0) {
+            cb(error, result[0]);
+        } else {
+            cb(error, null);
+        }
+    });
+}
+
 router.get('/', (req, res) => {
     res.status(200).send("Unauthorized Access!");
+});
+
+router.post("/login", cors(corsOptions), (req, res) => {
+    const email = req.body.email;
+    const senha = req.body.senha;
+
+    findUserByEmail(email, (err, user) => {
+        if (err) {
+            return res.status(500).send("Houve um erro no servidor: findUserByEmail");
+        }
+
+        if (!user) {
+            return res.status(404).send("Usuario não encontrado!");
+        }
+
+        const result = bcrypt.compareSync(senha, user.SENHA);
+
+        if (!result) {
+            return res.status(401).send("Senha inválida!");
+        } else {
+            delete user.SENHA;
+        }
+
+        if (user.ATIVO == 0) {
+            return res.status(999).send("Conta Dessativada!");
+        }
+
+        if (user.BLOQUEADO == 1) {
+            return res.status(998).send("Conta Bloqueada!");
+        }
+
+        const expiresIn = 7 * 24 * 60 * 60;
+        const accessToken = jwt.sign({ id: user.IDUSUARIO }, SECRET_KEY, { expiresIn: expiresIn });
+        res.status(200).send({ "user": user, "TOKEN": accessToken, "EXPIRE": expiresIn })
+    });
 });
 
 app.use(router);
